3ac24d7b10c3a9a6f317597d168446e7c9004c7e
adlsdztony <zzl0712@outlook.com>
2023-04-08 19:54:35 +0800

    3

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,34 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
 use std::io::{Read, Write};
+use std::net::TcpListener;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
-    
+
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
-    loop {
-        for stream in listener.incoming() {
-            match stream {
-                Ok(mut stream) => {
-                    println!("accepted new connection");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                println!("accepted new connection");
 
-                    let mut buf = [0; 512];
-                    stream.read(&mut buf).unwrap();
+                let mut buf = [0; 512];
+                loop {
+                    let bytes_read = stream.read(&mut buf).unwrap();
+                    if bytes_read == 0 {
+                        println!("client closed the connection");
+                        break;
+                    }
 
                     stream.write("+PONG\r\n".as_bytes()).unwrap();
                 }
-                Err(e) => {
-                    println!("error: {}", e);
-                }
+            }
+            Err(e) => {
+                println!("error: {}", e);
             }
         }
-    }   
+    }
 }

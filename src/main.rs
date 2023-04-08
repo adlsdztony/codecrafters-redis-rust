de0f293470bc167df1f87607ec4719076bed5ff4
adlsdztony <zzl0712@outlook.com>
2023-04-08 19:50:01 +0800

    3

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,22 @@ fn main() {
     // Uncomment this block to pass the first stage
     
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
 
-                let mut buf = [0; 512];
-                stream.read(&mut buf).unwrap();
+    loop {
+        for stream in listener.incoming() {
+            match stream {
+                Ok(mut stream) => {
+                    println!("accepted new connection");
 
-                stream.write("+PONG\r\n".as_bytes()).unwrap();
-            }
-            Err(e) => {
-                println!("error: {}", e);
+                    let mut buf = [0; 512];
+                    stream.read(&mut buf).unwrap();
+
+                    stream.write("+PONG\r\n".as_bytes()).unwrap();
+                }
+                Err(e) => {
+                    println!("error: {}", e);
+                }
             }
         }
-    }
+    }   
 }

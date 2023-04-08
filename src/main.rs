5950d0ede3671f0a04543c9a09d50c966943aa30
adlsdztony <zzl0712@outlook.com>
2023-04-08 19:46:14 +0800

    2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // Uncomment this block to pass the first stage
 use std::net::TcpListener;
+use std::io::{Read, Write};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,8 +12,13 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+
+                let mut buf = [0; 512];
+                stream.read(&mut buf).unwrap();
+
+                stream.write("+PONG\r\n".as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
